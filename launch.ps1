2c285e0d487f6c04fc40af9346d67fc4eda06717
David Turner <david.turner@zupa.co.uk>
2020-02-25 12:23:29 +0000

    logic to teardown processes once you're done.

--- launch.ps1
+++ launch.ps1
@@ -7,12 +7,18 @@ $config = Get-Content config.json | ConvertFrom-Json
 
 Set-Location -Path $config.WorkingDirectory
 
+$ProcessesList = [System.Collections.ArrayList]@()
 $config.ProcessesToRun | ForEach-Object -Process {
   $this = $config.Processes.$_
 
   $args = "run --project $($this.ProjectDirectory)"
   $errorPath = Join-Path -Path $errorDir -ChildPath "$_.out.txt"
-  Start-Process -FilePath "dotnet" -ArgumentList $args -RedirectStandardError $errorPath
+  $ProcessesList += Start-Process -FilePath "dotnet" -ArgumentList $args -RedirectStandardError $errorPath -PassThru
 }
 
 Set-Location -Path $startDir
+
+[void](Read-Host 'Press Enter to teardown...')
+
+Stop-Process -InputObject $ProcessesList
+Wait-Process -InputObject $ProcessesList
